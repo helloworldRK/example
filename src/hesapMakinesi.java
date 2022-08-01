5f6c21c85315d8cb5f4dca62f3e5effeda1cb398
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-08-01 12:56:50 +0300

    kullanıcıgirisi

--- src/hesapMakinesi.java
+++ src/hesapMakinesi.java
@@ -1,3 +1,40 @@
-public class hesapMakinesi {
+import java.util.Scanner;
 
+public class hesapMakinesi {
+    public static void main(String[] args) {
+        int islem,x,y;
+       Scanner input= new Scanner(System.in);
+
+        System.out.println( "ilk sayinizi giriniz");
+        x= input.nextInt();
+        System.out.println( "ikinci sayinizi giriniz");
+        y= input.nextInt();
+
+        System.out.println( "toplam 1\n cikarma 2\n carpma 3\n bolme 4 sekilde  seciniz");
+
+        System.out.println( "Islem : ");
+        islem= input.nextInt();
+        switch (islem){
+
+            case 1 :
+                System.out.println( "Toplam : " + (x + y));
+                break;
+            case 2 :
+                System.out.println( "Cıkarma : " + (x- y));
+                break;
+            case 3 :
+                System.out.println( "Carpma : " + (x * y));
+                break;
+            case 4 :
+                if (x != 0){
+                    System.out.println("Bolumu: "+ x / y);
+                }
+                else {
+                    System.out.println("0 bulunmez");
+                }
+                break;
+            default : System.out.println("HEY! Yanlıs tercih ");
+        }
+
+    }
 }
---
+++ src/kullaniciGirisi.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class kullaniciGirisi {
+    public static void main(String[] args) {
+        /*Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun,
+        eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre ile aynı olmaması gerektiğini
+        kontrol edip ,
+        şifreler aynı ise ekrana "Şifre oluşturulamadı, lütfen başka şifre giriniz."
+        sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.*/
+        String uN, passW, pass;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lutfen Kullanici Adinizi Giriniz:");
+        uN = input.next();
+
+        System.out.println("Lutfen Sifrenizi Giriniz:");
+        passW = input.next();
+
+        if (uN.equals("rk") && passW.equals("345")) {
+            System.out.println("Giris yaptiniz.");
+        }
+        else if   (uN==" "){
+            System.out.println("Kullanici Adi Bos!");
+        }
+        else if (passW.equals("345") == false)
+        {
+            System.out.println("Sifrenizi sifirlamak isterseniz 1'i\nSifirlamak istemezseniz 2'i seciniz.");
+
+            int devam;
+            Scanner inp = new Scanner(System.in);
+            devam = inp.nextInt();
+            if (devam == 1) {
+                System.out.print("Lutfen  Yeni Sifrenizi Giriniz:");
+                pass = input.nextLine();
+                if (pass.equals("345") == true) {
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz");
+                } else if (pass != "345"){
+                    pass = input.nextLine();
+                System.out.println("Yeni sifreniz olusturulmustur.");
+
+            }
+
+                else {
+                    System.out.println("Sifre oluşturuldu.");
+                }
+            }
+            else {
+                System.out.println("Sifrenizi degistirmek istemediginizi belirttiniz.");
+            }
+        }
+    }
+}
